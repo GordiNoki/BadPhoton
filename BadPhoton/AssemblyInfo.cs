43bd99950c80b2f75fb0477032f08a0b07df6a2b
GordiNoki <gordinoki8@gmail.com>
2024-09-14 11:28:50 +0300

    Добавьте файлы проекта.

---
+++ BadPhoton/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using BadPhoton;
+
+[assembly: MelonInfo(typeof(Mod), "BadPhoton", "1.0", "GeN")]
+[assembly: MelonGame("TECOPARK", "PICO PARK 2")]
---
+++ BadPhoton/Mod.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2CppPhoton.Realtime;
+
+namespace BadPhoton
+{
+    public class Mod : MelonMod
+    {
+        private MelonPreferences_Category badPhotonCategory;
+        private MelonPreferences_Entry<string> appId;
+
+        [HarmonyPatch(typeof(LoadBalancingClient), "Connect")]
+        [HarmonyPatch(typeof(LoadBalancingClient), "ConnectUsingSettings")]
+        static class AuthMethodPatch
+        {
+            public static void Prefix(LoadBalancingClient __instance) {
+                __instance.AuthValues = null;
+            }
+        }
+
+        public override void OnInitializeMelon()
+        {
+            badPhotonCategory = MelonPreferences.CreateCategory("Bad Photon");
+            appId = badPhotonCategory.CreateEntry("AppId", "17f87ebc-2af8-4da0-9e9f-a9e75a67e030"); // Default is official pico park 2 app id
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            Il2CppGame.ReleaseAppId.AppIdInternal = Il2CppCommon.AesEncryptor.EncryptString(appId.Value);
+            LoggerInstance.Msg($"Set Photon app id to encrypted {appId.Value}: {Il2CppGame.ReleaseAppId.AppIdInternal}");
+        }
+    }
+}
